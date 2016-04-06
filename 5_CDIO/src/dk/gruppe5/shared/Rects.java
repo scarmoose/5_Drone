e71314b7bb86319efa78e8fd01236ede12337664
Martin Vieth <martin_123_vieth@hotmail.com>
2016-04-06 21:43:40 +0200

    temp klasse til firkants data

---
+++ 5_CDIO/src/dk/gruppe5/shared/Rects.java
@@ -0,0 +1,37 @@
+package dk.gruppe5.shared;
+
+import org.opencv.core.Point;
+
+public class Rects {
+	
+	double area;
+	Point tlPoint;
+	Point brPoint;
+	
+	public Rects( double area, Point tlPoint, Point brPoint){
+		this.area = area;
+		this.tlPoint = tlPoint;
+		this.brPoint = brPoint;
+		
+	}
+	
+	public double getArea() {
+		return area;
+	}
+
+	public Point getTlPoint() {
+		return tlPoint;
+	}
+
+	public Point getBrPoint() {
+		return brPoint;
+	}
+	
+	public double getWidth(){
+		return brPoint.x-tlPoint.x;
+	}
+	public double getHeight(){
+		return brPoint.y-tlPoint.y;
+	}
+
+}
