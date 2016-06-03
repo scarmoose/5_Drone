b41ccb99e988b7783c77c3b6f2db6913b3feee89
Paulin Delvaux <Poll3xd@gmail.com>
2016-06-03 17:18:13 +0200

    omvendt model af rum

--- 5_CDIO/src/dk/gruppe5/CoordinateSystem/Lines.java
+++ 5_CDIO/src/dk/gruppe5/CoordinateSystem/Lines.java
@@ -1,8 +1,11 @@
 package dk.gruppe5.CoordinateSystem;
 
 import java.awt.EventQueue;
+import java.util.ArrayList;
 
 import javax.swing.JFrame;
+
+import dk.gruppe5.controller.Mathmagic;
 
 public class Lines extends JFrame {
 
@@ -18,7 +21,7 @@ public class Lines extends JFrame {
 
         setTitle("Lines");
         //Size of window
-        setSize(1150/2, 1100/2);
+        setSize(1000/2+80, 1150/2+80);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
@@ -26,9 +29,23 @@ public class Lines extends JFrame {
     public static void main(String[] args) {
 
         EventQueue.invokeLater(new Runnable() {
-            @Override
+			@Override
             public void run() {
                 
+				/*ArrayList<Integer> Xelements = new ArrayList<>();
+				ArrayList<Integer> Yelements = new ArrayList<>();
+				for (int i = 0 ; i < Mathmagic.getArray().length ; i++){
+					Xelements.add((int) Mathmagic.getArray()[i].getX());
+					Yelements.add((int) Mathmagic.getArray()[i].getY());
+				}
+            	
+				for (int k = 0;k< Xelements.size();k++){
+					int xvalue = Xelements.get(k);
+					int yvalue = Yelements.get(k);
+					System.out.println("x = " + xvalue + ", y= "+yvalue);
+				}*/
+				
+            	//System.out.println(Mathmagic.getArray()[0].getX());
                 Lines ex = new Lines();
                 ex.setVisible(true);
             }
--- 5_CDIO/src/dk/gruppe5/CoordinateSystem/Surface.java
+++ 5_CDIO/src/dk/gruppe5/CoordinateSystem/Surface.java
@@ -1,7 +1,10 @@
 package dk.gruppe5.CoordinateSystem;
 
+import java.awt.BasicStroke;
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.util.ArrayList;
 
 import javax.swing.JPanel;
 import dk.gruppe5.controller.Mathmagic;
@@ -15,14 +18,31 @@ class Surface extends JPanel {
         Graphics2D g2d = (Graphics2D) g;
 
         //The four walls
-        g2d.drawLine(0, 0, 1100/2, 0);
-        g2d.drawLine(0, 0, 0, 1000/2);
-        g2d.drawLine(0, 1000/2, 1100/2, 1000/2);
-        g2d.drawLine(1100/2, 1000/2, 1100/2, 0);
-       
+        g2d.drawLine(20, 50, 930/2+20, 50);
+        g2d.drawLine(20, 50, 20, 1060/2+50);
+        g2d.drawLine(20, 1060/2+50, 930/2+20, 1060/2+50);
+        g2d.drawLine(930/2+20, 1060/2+50, 930/2+20, 50);
+      
         
-        System.out.println(math.getArray()[1].getX());
+        ArrayList<Integer> Xelements = new ArrayList<>();
+		ArrayList<Integer> Yelements = new ArrayList<>();
+		for (int i = 0 ; i < Mathmagic.getArray().length ; i++){
+			Xelements.add((int) Mathmagic.getArray()[i].getX());
+			Yelements.add((int) Mathmagic.getArray()[i].getY());
+		}
+    	
+		for (int k = 0;k< Xelements.size();k++){
+			int xvalue = Xelements.get(k);
+			int yvalue = Yelements.get(k);
+			BasicStroke bs1 = new BasicStroke(8, BasicStroke.CAP_BUTT,
+			        BasicStroke.JOIN_BEVEL);
+			g2d.setStroke(bs1);
+			g2d.setPaint(Color.red);
+			g2d.drawRoundRect(xvalue/2+20, yvalue/2+50, 2, 2, 2, 2);
+			System.out.println("x = " + xvalue + ", y= "+yvalue);
+		}
         
+        g2d.setPaint(Color.blue);
     }
 
     @Override
