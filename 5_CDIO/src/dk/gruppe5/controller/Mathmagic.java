d0090dbce7c53e79fc662b6e6c55fb96f9b3dd05
thomas <thomas@liljegren.dk>
2016-06-13 16:19:38 +0200

    nye vægmarkeringsmålinger

--- 5_CDIO/src/dk/gruppe5/controller/Mathmagic.java
+++ 5_CDIO/src/dk/gruppe5/controller/Mathmagic.java
@@ -19,7 +19,7 @@ public class Mathmagic {
 	public final static Wallmark[] wallmarks = {
 			new Wallmark("W00.00",new Point(188,1055)), new Wallmark("W00.01",new Point(338, 1060)), new Wallmark("W00.02",new Point(515,1055)), new Wallmark("W00.03",new Point(694, 1060)), new Wallmark("W00.04",new Point(840, 1055)),
 			new Wallmark("W01.00",new Point(926, 904)), new Wallmark("W01.01",new Point(926, 721)), new Wallmark("W01.02",new Point(926, 566)), new Wallmark("W01.03",new Point(926, 324)), new Wallmark("W01.04",new Point(926, 115)),
-			new Wallmark("W02.00",new Point(847, -10)), new Wallmark("W02.01",new Point(656, -77)), new Wallmark("W02.02",new Point(420, 0)), new Wallmark("W02.03",new Point(350, 0)), new Wallmark("W02.04",new Point(150,0)),
+			new Wallmark("W02.00",new Point(847, -10)), new Wallmark("W02.01",new Point(656, -77)), new Wallmark("W02.02",new Point(514, 0)), new Wallmark("W02.03",new Point(328, 0)), new Wallmark("W02.04",new Point(143,0)),
 			new Wallmark("W03.00",new Point(0,108)), new Wallmark("W03.01",new Point(0, 357)), new Wallmark("W03.02",new Point(0, 561)),new Wallmark("W03.03", new Point(0,770)), new Wallmark("W03.04",new Point(0,997)) 
 	};
 
